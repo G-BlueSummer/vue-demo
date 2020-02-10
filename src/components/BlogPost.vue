011f1e8f56f0f8c9e08bd26ab2a7dd36ecb3912b
iii <cclan8@outlook.com>
2020-02-10 21:29:42 +0800

    pagination

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="container">
     <div class="section">
+      <ul class="pagination">
+        <li :class="[currentPage===1?'disabled':'waves-effect']">
+          <a @click="subPage"><i class="material-icons">chevron_left</i></a>
+        </li>
+        <li v-for="i in maxPage" :key="i" :class="[i===currentPage?'active':'waves-effect']">
+          <a>{{i}}</a>
+        </li>
+        <li :class="[currentPage===maxPage?'disabled':'waves-effect']">
+          <a @click="addPage"><i class="material-icons">chevron_right</i></a>
+        </li>
+      </ul>
+
       <ul class="collection">
-        <li class="collection-item" v-for="blog in blogs" v-bind:key="blog.id">
+        <li class="collection-item" v-for="blog in blogs.slice(start, end)" :key="blog.id">
           <h4>{{blog.title}}</h4>
         </li>
       </ul>
@@ -11,10 +23,12 @@
 </template>
 
 <script>
+const pageSize = 10;
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      currentPage: 1,
     }
   },
   mounted() {
@@ -22,5 +36,14 @@ export default {
       .then(r => (this.blogs = r.data))
       .catch(e => (console.error(e)))
   },
+  computed: {
+    maxPage() { return this.blogs.length / pageSize },
+    start() { return (this.currentPage - 1) * pageSize },
+    end() { return this.currentPage * pageSize },
+  },
+  methods: {
+    addPage() { if (this.currentPage !== this.maxPage) this.currentPage++ },
+    subPage() { if (this.currentPage !== 1) this.currentPage-- },
+  }
 }
 </script>
