c99487d340e7c9e4ec9d8b523e213e04a3704142
iii <cclan8@outlook.com>
2020-02-13 18:11:04 +0800

    add preload

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="container">
     <div class="section">
-      <ul class="pagination">
-        <li :class="[canPrev?'waves-effect':'disabled']">
-          <a @click="prevPage"><i class="material-icons">chevron_left</i></a>
-        </li>
-        <li v-for="i in maxPage" :key="i" :class="{'active':i===currentPage}">
-          <a @click="changePage(i)">{{i}}</a>
-        </li>
-        <li :class="[canNext?'waves-effect':'disabled']">
-          <a @click="nextPage"><i class="material-icons">chevron_right</i></a>
-        </li>
-      </ul>
-
-      <ul class="collection">
-        <li class="collection-item" v-for="blog in blogs.slice(start, end)" :key="blog.id">
-          <h4>{{blog.title}}</h4>
-        </li>
-      </ul>
+      <div class="progress" v-if="loading">
+          <div class="indeterminate"></div>
+      </div>
+      <div v-else>
+        <ul class="pagination">
+          <li :class="[canPrev?'waves-effect':'disabled']">
+            <a @click="prevPage"><i class="material-icons">chevron_left</i></a>
+          </li>
+          <li v-for="i in maxPage" :key="i" :class="[i===currentPage?'active':'waves-effect']">
+            <a @click="changePage(i)">{{i}}</a>
+          </li>
+          <li :class="[canNext?'waves-effect':'disabled']">
+            <a @click="nextPage"><i class="material-icons">chevron_right</i></a>
+          </li>
+        </ul>
+      
+        <ul class="collection">
+          <li class="collection-item" v-for="blog in blogs.slice(start, end)" :key="blog.id">
+            <h4>{{blog.title}}</h4>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -29,12 +34,13 @@ export default {
     return {
       blogs: [],
       currentPage: 1,
+      loading: true,
     }
   },
   mounted() {
     this.$axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(r => (this.blogs = r.data))
-      .catch(e => (alert(e)))
+      .then( r => { this.blogs = r.data; this.loading = false } )
+      .catch( e => { alert(e) } )
   },
   computed: {
     maxPage() { return Math.ceil(this.blogs.length / pageSize) },
